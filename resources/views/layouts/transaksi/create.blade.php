ffd3036a94b9c13abe5e85decb65569cb9f06440
Deby <deborasopiana47@gmail.com>
2022-07-09 12:10:10 +0800

    add searchable select using select2

--- resources/views/layouts/transaksi/create.blade.php
+++ resources/views/layouts/transaksi/create.blade.php
@@ -9,6 +9,13 @@
     }
 </style>
 
+<head>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+</head>
+
 <body>
     <nav class="sidenav navbar navbar-verticaL fixed-left navbar-expand-xs navbar-light" id="sidenav-main">
         <div class="scrollbar-inner">
@@ -49,9 +56,9 @@
             <div class="header-body mt-7 mb-7">
                 <div class="col-xs-6" style="position:fixed; left: 300px; right: 80px;">
                     <form method="POST" class="box-form">
+                    <label for="id_pegawai">Pegawai</label>
                         <div class="form-group">
-                            <label for="id_pegawai">Pegawai</label>
-                            <select class="custom-select" name="id_pegawai" id="id_pegawai" required>
+                            <select class="custom-select" name="id_pegawai" id="id_pegawai" style="width:100%" required>
                                 <option selected>Pilih Pegawai</option>
                                 @foreach (App\Models\Pegawai::selectPegawai()->sortBy('nama') as $pg)
                                 <option value="{{ $pg->id }}">{{ $pg->nama . " - " . $pg->nip . " - " . $pg->instansi}}</option>
@@ -93,26 +100,23 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script>
-        $(document).ready(function() {
-            $('#id_pegawai').on('change', function() {
-                var pegawaiID = $(this).val();
-                if (pegawaiID) {
-                    $.ajax({
-                        url: '/trx/kuota/' + pegawaiID,
-                        dataType: 'JSON',
-                        type: "GET",
-                        data: {
-                            "id": pegawaiID
-                        },
-                        success: function(response) {
-                            document.getElementById('kuota').value = response;
-                        }
-                    });
-                }
-            });
+        $('#id_pegawai').select2();
+        $('#id_pegawai').on('change', function() {
+            var pegawaiID = $(this).val();
+            if (pegawaiID) {
+                $.ajax({
+                    url: '/trx/kuota/' + pegawaiID,
+                    dataType: 'JSON',
+                    type: "GET",
+                    data: {
+                        "id": pegawaiID
+                    },
+                    success: function(response) {
+                        document.getElementById('kuota').value = response;
+                    }
+                });
+            }
         });
     </script>
 </body>
